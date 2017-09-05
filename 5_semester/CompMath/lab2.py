d9dcfe1d1b1e9e1eda5afce087d4f0ccd2b27912
Alexey Terentyev <alexey@terentyev.pro>
2017-09-05 19:10:15 +0700

    #1 Trying to slove CompMath

--- 5_semester/CompMath/lab2.py
+++ 5_semester/CompMath/lab2.py
@@ -1,3 +1,6 @@
+import math
+import my_error
+
 N = 2
 C = 0.01 * N
 
@@ -33,10 +36,79 @@ def norm_second(array):
         norm.append(listsum(array[i], 2))
     return round(listsum(norm) ** (1/2), 4)
 
+
+def matrix_diagonal_division(array, mod=1):
+    """
+    Divides each line of the system by the corresponding diagonal element of the matrix, which later multiplies by mod
+    :param array: array Source array
+    :param mod: int Factor
+    :return: array
+    """
+    if len(array) == 0 or len(array) != len(array[0]):
+        return None
+    for i in range(len(array)):
+        value = array[i][i]
+        array[i][i] *= mod
+        for j in range(len(array[i])):
+            array[i][j] /= value
+    return array
+
+
+def matrix_multiplication(first_matrix, second_matrix):
+    third_matrix = []
+    for i in range(len(first_matrix)):
+        for j in range(len(second_matrix[0])):
+            value = 0
+            for k in range(len(second_matrix)):
+                value += first_matrix[i][k] * second_matrix[k][j]
+            #third_matrix[i][j] = value
+            #print(str(value))
+    return third_matrix
+
+
+def iterations_number(norm_b, norm_c, r=0.01):
+    try:
+        if my_error.SubZeroError.check(norm_b, norm_c) == 0:
+            raise my_error.SubZeroError(norm_b, norm_c)
+    except my_error.SubZeroError as e:
+        print(e.msg)
+    else:
+        number = math.log((r * (1 - norm_c)) / norm_b) / math.log(norm_c)
+        if number // 2 > 0:
+            return round(number) - 1
+        else:
+            return number
+
+
+def matrix_reduction(array):
+    if len(array) == 0 or len(array) != len(array[0]):
+        return None
+    for i in range(len(array)):
+        array[i][i] = 0
+    return array
+
+
 test_array = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
+]
+
+test_array2 = [
+    [10, -1, 4],
+    [3, 10, -5],
+    [-1, -3, 5]
+]
+
+a = [
+    [-1, 1],
+    [2, 0],
+    [0, 3]
+]
+
+b = [
+    [3, 1, 2],
+    [0, -1, 4]
 ]
 
 a_array = [
@@ -57,7 +129,13 @@ for i in range(len(a_array)):
     for j in range(len(a_array[i])):
         a_array[i][j] = round(a_array[i][j], 4)
 #print(my_array)
-
+"""
 print(norm_first(test_array))
 print(norm_second(test_array))
 print(norm_infinite(test_array))
+"""
+#print(matrix_diagonal_division(test_array))
+#print(str(iterations_number(norm_second(b_array), norm_second(matrix_diagonal_division(a_array)))))
+#print(str(iterations_number(1.2, 0.8)))
+#print(matrix_diagonal_division(test_array2, 0))
+print(matrix_multiplication(a, b))
