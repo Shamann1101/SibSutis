69992407e90a6444f19ab67ebeb28be1c11aadc2
Alexey Terentyev <alexey@terentyev.pro>
2017-09-07 18:19:53 +0700

    #1 Lab2 in progress

--- 5_semester/CompMath/lab2.py
+++ 5_semester/CompMath/lab2.py
@@ -5,23 +5,23 @@ N = 2
 C = 0.01 * N
 
 
-def listsum(num_list, mod=1):
+def list_sum(num_list, mod=1):
     """
     Sum of elements in the series
     :param num_list: array
-    :param mod: int
+    :param mod: int The degree to which members of the series
     :return: int|real
     """
     the_sum = 0
     for i in num_list:
-        the_sum += i ** mod
+        the_sum += math.fabs(i) ** mod
     return the_sum
 
 
 def norm_infinite(array):
     norm = []
     for i in range(len(array)):
-        norm.insert(i, listsum(array[i]))
+        norm.insert(i, list_sum(array[i]))
     return max(norm)
 
 
@@ -33,37 +33,86 @@ def norm_first(array):
 def norm_second(array):
     norm = []
     for i in range(len(array)):
-        norm.append(listsum(array[i], 2))
-    return round(listsum(norm) ** (1/2), 4)
+        norm.append(list_sum(array[i], 2))
+    return round(math.sqrt(list_sum(norm)), 4)
 
 
-def matrix_diagonal_division(array, mod=1):
-    """
-    Divides each line of the system by the corresponding diagonal element of the matrix, which later multiplies by mod
-    :param array: array Source array
-    :param mod: int Factor
-    :return: array
-    """
-    if len(array) == 0 or len(array) != len(array[0]):
-        return None
-    for i in range(len(array)):
-        value = array[i][i]
-        array[i][i] *= mod
-        for j in range(len(array[i])):
-            array[i][j] /= value
-    return array
+def get_matrix_diagonal_values(matrix):
+    try:
+        if len(matrix) == 0:
+            raise ValueError
+    except ValueError:
+        print("The matrix can not be empty")
+    else:
+        new_matrix = [0] * len(matrix)
+        for i in range(len(matrix)):
+            new_matrix[i] = [0]
+            new_matrix[i][0] = matrix[i][i]
+        return new_matrix
+
+
+def set_matrix_diagonal_values(matrix, value=0):
+    try:
+        if len(matrix) == 0:
+            raise ValueError
+    except ValueError:
+        print("The matrix can not be empty")
+    else:
+        new_matrix = matrix
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                new_matrix[i][i] = value
+        return new_matrix
 
 
 def matrix_multiplication(first_matrix, second_matrix):
-    third_matrix = []
-    for i in range(len(first_matrix)):
-        for j in range(len(second_matrix[0])):
-            value = 0
-            for k in range(len(second_matrix)):
-                value += first_matrix[i][k] * second_matrix[k][j]
-            #third_matrix[i][j] = value
-            #print(str(value))
-    return third_matrix
+    try:
+        if len(first_matrix[0]) != len(second_matrix):
+            raise Exception
+    except Exception:
+        print("The dimension of the matrices is not suitable for multiplication")
+    else:
+        new_matrix = [0] * len(first_matrix)
+        for i in range(len(first_matrix)):
+            new_matrix[i] = [0] * len(second_matrix[0])
+            for j in range(len(second_matrix[0])):
+                value = 0
+                for k in range(len(second_matrix)):
+                    value += first_matrix[i][k] * second_matrix[k][j]
+                new_matrix[i][j] = value
+        return new_matrix
+
+
+def matrix_multiplication_by_number(first_matrix, value, division=0):
+    try:
+        if type(value) != list and type(value) != int:
+            raise TypeError
+    except TypeError:
+        print("Value must be list or int type")
+    else:
+        new_matrix = [0] * len(first_matrix)
+
+        if type(value) == list:
+            if (len(first_matrix)) == 1:
+                first_matrix = list(zip(*first_matrix))
+            for i in range(len(first_matrix)):
+                new_matrix[i] = [0] * len(first_matrix[i])
+                for j in range(len(first_matrix[i])):
+                    if division == 0:
+                        new_matrix[i][j] = first_matrix[i][j] * value[i][0]
+                    else:
+                        new_matrix[i][j] = first_matrix[i][j] / value[i][0]
+
+        elif type(value) == int:
+            for i in range(len(first_matrix)):
+                new_matrix[i] = [0] * len(first_matrix[i])
+                for j in range(len(first_matrix[i])):
+                    if division == 0:
+                        new_matrix[i][j] = first_matrix[i][j] * value
+                    else:
+                        new_matrix[i][j] = first_matrix[i][j] / value
+
+        return new_matrix
 
 
 def iterations_number(norm_b, norm_c, r=0.01):
@@ -80,14 +129,6 @@ def iterations_number(norm_b, norm_c, r=0.01):
             return number
 
 
-def matrix_reduction(array):
-    if len(array) == 0 or len(array) != len(array[0]):
-        return None
-    for i in range(len(array)):
-        array[i][i] = 0
-    return array
-
-
 test_array = [
     [1, 2, 3],
     [4, 5, 6],
@@ -98,6 +139,10 @@ test_array2 = [
     [10, -1, 4],
     [3, 10, -5],
     [-1, -3, 5]
+]
+
+test_array3 = [
+    [1], [0], [6]
 ]
 
 a = [
@@ -134,8 +179,16 @@ print(norm_first(test_array))
 print(norm_second(test_array))
 print(norm_infinite(test_array))
 """
-#print(matrix_diagonal_division(test_array))
-#print(str(iterations_number(norm_second(b_array), norm_second(matrix_diagonal_division(a_array)))))
-#print(str(iterations_number(1.2, 0.8)))
-#print(matrix_diagonal_division(test_array2, 0))
-print(matrix_multiplication(a, b))
+
+c_array = matrix_multiplication_by_number(test_array2, get_matrix_diagonal_values(test_array2), 1)
+c_array = set_matrix_diagonal_values(c_array)
+c_norm = norm_infinite(c_array)
+b_array = matrix_multiplication_by_number(test_array3, get_matrix_diagonal_values(test_array2), 1)
+b_norm = norm_infinite(b_array)
+n = iterations_number(b_norm, c_norm)
+
+print(c_array)
+print(str(c_norm))
+print(b_array)
+print(str(b_norm))
+print(str(n))
