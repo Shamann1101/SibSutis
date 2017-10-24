e828b77593ee7b8913afbf4414ac7b1f5e064a9f
user432 <user432@cyber.sibsutis.ru>
2017-10-24 15:33:37 +0700

    #1 Lab1 edited

--- 5_semester/CompMath/lab1.py
+++ 5_semester/CompMath/lab1.py
@@ -27,12 +27,41 @@ def func_i(i):
     return round(C + (i * H) + (mod4(i) + 1) / 5 * H, 4)
 
 
-i = float(C)
-while i <= C + 30 * H:
-    print("x = " + str(i) + " func = " + str(func(i)))
-    i = round(i + H, 4)
+def table_create(c=C, h=H):
+    table = dict()
+    i = float(c)
+    while i <= C + 30 * h:
+        table[i] = func(i)
+        i = round(i + h, 4)
+    return table
 
 
-for n in range(30):
-    x = func_i(n)
-    print("x" + str(n) + " = " + str(x) + " func = " + str(func(x)))
+def find_nearest(table, x):
+    value = None
+    next_elem = True
+    for elem in table.keys():
+        if elem >= x and next_elem == True:
+            value = [elem, table[elem]]
+            next_elem = False
+        elif elem >= x and next_elem == False:
+            value.append(elem)
+            print(value)
+            return value
+        
+
+def interpolation(x, y, nearest_pair, h=H):
+    q = (x - nearest_pair[0]) / h
+    inter = nearest_pair[1] + q * (nearest_pair[2] - nearest_pair[1])
+    return round(inter, 4)
+
+
+def iter_table_create(c=C, h=H):
+    table = dict()
+    for n in range(30):
+        x = func_i(n)
+        table[x] = [interpolation(x, func(x), find_nearest(table_create(), x), h), func(x)]
+    return table
+
+
+print(table_create())
+print(iter_table_create())
