3d759f190102a59f1e57d1b0e75a9c48edd3fc60
Terentyev Alexey <alexey@terentyev.pro>
2017-10-24 22:30:09 +0700

    #1 Lab1 is Done

--- 5_semester/CompMath/lab1.py
+++ 5_semester/CompMath/lab1.py
@@ -28,6 +28,12 @@ def func_i(i):
 
 
 def table_create(c=C, h=H):
+    """
+    Creates a table of values of the function with the calculated step h on the interval [c, c + 30h]
+    :param c:
+    :param h:
+    :return:
+    """
     table = dict()
     i = float(c)
     while i <= C + 30 * h:
@@ -37,29 +43,50 @@ def table_create(c=C, h=H):
 
 
 def find_nearest(table, x):
-    value = None
+    """
+    Looks for the nearest value of an argument and a function
+    :param table:
+    :param x:
+    :return:
+    """
+    value = list()
     next_elem = True
     for elem in table.keys():
         if elem >= x and next_elem == True:
-            value = [elem, table[elem]]
+            value.insert(0, elem)
+            value.insert(1, table[elem])
+            value.insert(2, table[elem])
             next_elem = False
         elif elem >= x and next_elem == False:
-            value.append(elem)
-            print(value)
+            value.pop(2)
+            value.insert(2, table[elem])
             return value
-        
+    return value
 
-def interpolation(x, y, nearest_pair, h=H):
+
+def interpolation(x, nearest_pair, h=H):
+    """
+    Linear interpolation
+    :param x:
+    :param nearest_pair:
+    :param h:
+    :return:
+    """
     q = (x - nearest_pair[0]) / h
     inter = nearest_pair[1] + q * (nearest_pair[2] - nearest_pair[1])
     return round(inter, 4)
 
 
-def iter_table_create(c=C, h=H):
+def iter_table_create(h=H):
+    """
+    Creates a table of values of the interpolation
+    :param h:
+    :return:
+    """
     table = dict()
     for n in range(30):
         x = func_i(n)
-        table[x] = [interpolation(x, func(x), find_nearest(table_create(), x), h), func(x)]
+        table[x] = [interpolation(x, find_nearest(table_create(), x), h), func(x)]
     return table
 
 
