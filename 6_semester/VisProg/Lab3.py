2da07306acf7c01a3cd84c4cb267edaf650186b9
Terentyev Alexey <alexey@terentyev.pro>
2018-04-03 23:39:18 +0700

    VisProg in progress

--- 6_semester/VisProg/Lab3.py
+++ 6_semester/VisProg/Lab3.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import (QWidget, QAction, qApp, QPushButton, QLabel, QLineEdit,
+from PyQt5.QtWidgets import (QWidget, QAction, qApp, QPushButton, QFileDialog,
                              QTextEdit, QHBoxLayout, QVBoxLayout, QApplication)
 
 
 class Example(QWidget):
+    file = ""
 
     def __init__(self):
         super().__init__()
 
         self.initUI()
-
 
     def initUI(self):
         self.openButton = QPushButton("Open")
@@ -27,6 +27,20 @@ class Example(QWidget):
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
         self.quitButton.clicked.connect(qApp.quit)
+
+        openFile = QAction(self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.readFile)
+        self.openButton.clicked.connect(self.readFile)
+
+        closeFile = QAction(self)
+        closeFile.setShortcut('Ctrl+S')
+        closeFile.setStatusTip('Save File')
+        closeFile.triggered.connect(self.saveFile)
+        self.saveButton.clicked.connect(self.saveFile)
+
+        self.clearButton.clicked.connect(self.clearText)
 
         vbox = QVBoxLayout()
         vbox.addStretch(1)
@@ -45,6 +59,23 @@ class Example(QWidget):
         self.setGeometry(300, 300, 350, 300)
         self.setWindowTitle('Review')
         self.show()
+
+    def readFile(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
+        self.file = fname
+        if fname:
+            f = open(fname, 'r')
+            with f:
+                data = f.read()
+                self.reviewEdit.setText(data)
+
+    def saveFile(self):
+        if self.file:
+            with open(self.file, 'w') as file:
+                print(self.reviewEdit.toPlainText(), file=file, sep="\n")
+
+    def clearText(self):
+        self.reviewEdit.setText("")
 
 
 if __name__ == '__main__':
