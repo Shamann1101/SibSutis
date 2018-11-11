f6ee391b7cebf1c4b55d2c0397c60fa4b5dec96e
Alexey Terentyev <alexey@terentyev.pro>
2018-11-11 22:25:45 +0700

    Separate Graph class

--- 7_semester/complexityOfCompProcesses/dijkstra.py
+++ 7_semester/complexityOfCompProcesses/dijkstra.py
@@ -1,75 +1,16 @@
-from math import inf
 import argparse
-
-
-class Graph:
-    object_count = 0
-    object_visited = 0
-
-    @classmethod
-    def get_object_count(cls):
-        return cls.object_count
-
-    @classmethod
-    def get_object_visited(cls):
-        return cls.object_visited
-
-    @classmethod
-    def _increase_object_count(cls):
-        cls.object_count += 1
-
-    @classmethod
-    def _decrease_object_count(cls):
-        cls.object_count -= 1
-
-    @classmethod
-    def _increase_object_visited(cls):
-        cls.object_visited += 1
-
-    @classmethod
-    def _decrease_object_visited(cls):
-        cls.object_visited -= 1
-
-    def __init__(self, title):
-        self.title = title
-        self.neighbours = []
-        self.calculated_weight = inf
-        self.is_visited = False
-        self._increase_object_count()
-
-    def __str__(self):
-        return "Title: " + str(self.title)
-
-    def __del__(self):
-        self._decrease_object_count()
-        if self.is_visited:
-            self._decrease_object_visited()
-
-    @property
-    def neighbours(self):
-        # print("Getter")
-        return self._neighbours
-
-    @neighbours.setter
-    def neighbours(self, value):
-        # print("Setter")
-        self._neighbours = value
-
-    def set_visited(self):
-        if not self.is_visited:
-            self.is_visited = True
-            self._increase_object_visited()
+import graph
 
 
 def dijkstra(vertex_list, target_id):
     """
     Sets vertex weights by easiest way based on Dijkstra's algorithm
-    :param vertex_list: array of Graph objects
+    :param vertex_list: array of graph.Graph objects
     :param target_id: source id
     :return:
     """
     try:
-        if target_id < 0 or target_id > Graph.object_count:
+        if target_id < 0 or target_id > graph.Graph.object_count:
             raise ValueError
     except ValueError:
         print("Target id out os scope")
@@ -78,7 +19,7 @@ def dijkstra(vertex_list, target_id):
             print("=== Dijkstra's algorithm ===")
         path_list = {vertex_list[target_id]: 0}
         vertex_list[target_id].calculated_weight = 0
-        while Graph.object_visited < Graph.object_count and len(path_list) > 0:
+        while graph.Graph.object_visited < graph.Graph.object_count and len(path_list) > 0:
             nearest_graph = min(path_list, key=path_list.get)
             if args.print:
                 print(nearest_graph)
@@ -107,14 +48,14 @@ def dijkstra(vertex_list, target_id):
 def find_path(vertex_list, source_id, target_id, path=[]):
     msg = ""
     try:
-        if Graph.object_visited < Graph.object_count:
+        if graph.Graph.object_visited < graph.Graph.object_count:
             msg = "Need to execute `dijkstra` first"
-            print(Graph.object_visited, Graph.object_count)
+            print(graph.Graph.object_visited, graph.Graph.object_count)
             raise RuntimeError
-        if source_id < 0 or source_id > Graph.object_count:
+        if source_id < 0 or source_id > graph.Graph.object_count:
             msg = "source_id is out os scope"
             raise ValueError()
-        if target_id < 0 or target_id > Graph.object_count:
+        if target_id < 0 or target_id > graph.Graph.object_count:
             msg = "target_id is out os scope"
             raise ValueError()
     except ValueError:
@@ -151,7 +92,7 @@ def find_path(vertex_list, source_id, target_id, path=[]):
 def main():
     vertex_list = []
     for i in range(6):
-        vertex_list.append(Graph(i))
+        vertex_list.append(graph.Graph(i))
     vertex_list[0].neighbours = {vertex_list[1].title: 7,
                                  vertex_list[2].title: 9,
                                  vertex_list[5].title: 14}
@@ -171,8 +112,8 @@ def main():
                                  vertex_list[2].title: 2,
                                  vertex_list[4].title: 9}
 
-    source_vertex = int(args.source) if args.source and 0 <= args.source < Graph.object_count else 0
-    target_vertex = int(args.target) if args.target and 0 <= args.target < Graph.object_count else 4
+    source_vertex = int(args.source) if args.source and 0 <= args.source < graph.Graph.object_count else 0
+    target_vertex = int(args.target) if args.target and 0 <= args.target < graph.Graph.object_count else 4
 
     dijkstra(vertex_list, source_vertex)
 
