7d6364c38823dd9e4efdafe67eb5fea89e477260
Alexey Terentyev <alexey@terentyev.pro>
2018-11-12 15:38:15 +0700

    Floyd is Done

--- 7_semester/complexityOfCompProcesses/floyd.py
+++ 7_semester/complexityOfCompProcesses/floyd.py
@@ -1,7 +1,13 @@
+import argparse
 from graph import Graph
 
 
 def floyd(weight_matrix):
+    """
+    Algorithm for finding shortest paths in a weighted graph with positive or negative edge weights
+    :param weight_matrix:
+    :return: two-dimensional list with weights, two-dimensional list with paths
+    """
     error_msg = ""
     try:
         if len(weight_matrix) < 1:
@@ -30,13 +36,30 @@ def floyd(weight_matrix):
 
 
 def find_path(path_matrix, source_id, target_id):
-    path_list = list()
-    source = source_id
-    while path_matrix[source][target_id]:
-        print(path_matrix[source][target_id])
-        path_list.append(path_matrix[source_id][target_id])
-        source = path_matrix[source][target_id]
-    return path_list
+    """
+    Returns shortest way from source vertex to target vertex
+    :param path_matrix:
+    :param source_id:
+    :param target_id:
+    :return:
+    """
+    error_msg = ""
+    try:
+        if len(path_matrix) < 1:
+            error_msg = "Path matrix is empty"
+            raise ValueError
+        elif len(path_matrix) != len(path_matrix[0]):
+            error_msg = "Path matrix must be quadratic"
+            raise ValueError
+    except ValueError:
+        print(error_msg)
+    else:
+        target = target_id
+        path_list = [target]
+        while path_matrix[source_id][target] is not None:
+            path_list.append(path_matrix[source_id][target])
+            target = path_matrix[source_id][target]
+        return path_list
 
 
 def main():
@@ -71,10 +94,28 @@ def main():
                 weight_matrix_initial[line][vertex] = value
 
     wm, pm = floyd(weight_matrix_initial)
-    print(wm, "\n", pm)
+    if args.print:
+        print("Weights matrix:")
+        for line in wm:
+            print(line)
+        print()
+        print("Path matrix:")
+        for line in pm:
+            print(line)
+        print()
 
-    print(find_path(pm, 0, 4))
+    source_vertex = int(args.source) if args.source and 0 <= args.source < Graph.object_count else 0
+    target_vertex = int(args.target) if args.target and 0 <= args.target < Graph.object_count else 4
+
+    path = find_path(pm, source_vertex, target_vertex)
+    print("Path:", path)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Floyd’s algorithm')
+    parser.add_argument('source', type=int, nargs='?', help='Source vertex')
+    parser.add_argument('target', type=int, nargs='?', help='Target vertex')
+    parser.add_argument('--print', action='store_true', help='Print log')
+    args = parser.parse_args()
+
     main()
