259fa99654753cb2bd34bcb5507c622394103a8f
Alexey Terentyev <alexey@terentyev.pro>
2018-12-11 10:45:54 +0700

    Cleanup

--- 7_semester/computationalOptimizationMethods/course.py
+++ 7_semester/computationalOptimizationMethods/course.py
@@ -29,10 +29,10 @@ def find_minimal_co(arr):
 
 
 def is_positive_list(arr):
-    for i in arr:
-        if i < 0:
-            return False
-    return True
+    if min(arr) < 0:
+        return False
+    else:
+        return True
 
 
 def print_array(arr):
@@ -48,56 +48,51 @@ def transfiguration(source_data, index_row, index_col):
     i_row = index_row.copy()
     i_col = index_col.copy()
 
-    m_row = [i for i in data[-1]]
-    maximum_m, maximum_m_index = find_minimal_m(m_row[:-1], excluded_columns)
-    # maximum_m = min(m_row[:-1])
-    # maximum_m_index = m_row.index(maximum_m)
-    print("m_row:", m_row, maximum_m, maximum_m_index)
+    while not is_positive_list(data[-1][:-1]):
+        m_row = [i for i in data[-1]]
+        minimum_m, maximum_m_index = find_minimal_m(m_row[:-1], excluded_columns)
+        print("m_row:", m_row, minimum_m, maximum_m_index)
 
-    for row in range(len(i_col)):
-        # print(f'{source_data[row][-1]} / {source_data[row][maximum_m_index]}')
-        if len(data[row]) == len(i_row) + 1:
-            simplex = data[row][-1] / data[row][maximum_m_index]
-            data[row].append(simplex)
-        elif len(data[row]) == len(i_row) + 2:
-            if data[row][maximum_m_index] == 0:
-                print("zero:", row, maximum_m_index)
-            simplex = data[row][-2] / data[row][maximum_m_index]
-            data[row][-1] = simplex
-        else:
-            print("Something gone wrong")
-            return
+        for row in range(len(i_col)):
+            if len(data[row]) == len(i_row) + 1:
+                simplex = data[row][-1] / data[row][maximum_m_index]
+                data[row].append(simplex)
+            elif len(data[row]) == len(i_row) + 2:
+                if data[row][maximum_m_index] == 0:
+                    print("zero:", row, maximum_m_index)
+                simplex = data[row][-2] / data[row][maximum_m_index]
+                data[row][-1] = simplex
+            else:
+                print("Something gone wrong")
+                return
 
-    m_col = [data[i][-1] for i in range(len(i_col))]
-    minimum_co, minimum_co_index = find_minimal_co(m_col)
-    # minimum_co = min(m_col)
-    # minimum_co_index = m_col.index(minimum_co)
-    print("m_col:", m_col, minimum_co, minimum_co_index)
+        m_col = [data[i][-1] for i in range(len(i_col))]
+        minimum_co, minimum_co_index = find_minimal_co(m_col)
+        print("m_col:", m_col, minimum_co, minimum_co_index)
 
-    for row in range(len(data)):
-        if row == minimum_co_index:
-            continue
-        for elem in range(len(i_row) + 1):
-            if elem == maximum_m_index or elem in excluded_columns:
+        for row in range(len(data)):
+            if row == minimum_co_index:
                 continue
-            data[row][elem] -= data[row][maximum_m_index] * data[minimum_co_index][elem] / data[minimum_co_index][maximum_m_index]
-            # print(data[row][elem])
+            for elem in range(len(i_row) + 1):
+                if elem == maximum_m_index or elem in excluded_columns:
+                    continue
+                data[row][elem] -= data[row][maximum_m_index] * data[minimum_co_index][elem] / data[minimum_co_index][maximum_m_index]
 
-    base_value = data[minimum_co_index][maximum_m_index]
-    for row in range(len(data)):
-        for elem in range(len(i_row) + 1):
-            if elem in excluded_columns:
-                continue
-            elif row == minimum_co_index:
-                data[row][elem] /= base_value
-            elif elem == maximum_m_index:
-                data[row][elem] = 0
+        base_value = data[minimum_co_index][maximum_m_index]
+        for row in range(len(data)):
+            for elem in range(len(i_row) + 1):
+                if elem in excluded_columns:
+                    continue
+                elif row == minimum_co_index:
+                    data[row][elem] /= base_value
+                elif elem == maximum_m_index:
+                    data[row][elem] = 0
 
-    tmp = i_row[maximum_m_index]
-    i_row[maximum_m_index] = i_col[minimum_co_index]
-    i_col[minimum_co_index] = tmp
-    excluded_columns.append(maximum_m_index)
-    del tmp
+        tmp = i_row[maximum_m_index]
+        i_row[maximum_m_index] = i_col[minimum_co_index]
+        i_col[minimum_co_index] = tmp
+        excluded_columns.append(maximum_m_index)
+        del tmp
 
     print_array(data)
     print(i_row, i_col)
@@ -117,19 +112,7 @@ def main():
     index_row = [i for i in range(5)]
     index_col = [i for i in range(5, 8)]
 
-    # m_row = [fabs(i) for i in source_data[-1]]
-    # maximum_m = max(m_row[:-1])
-    # maximum_m_index = m_row.index(maximum_m)
-    #
-    # for row in range(len(index_col)):
-    #     # print(f'{source_data[row][-1]} / {source_data[row][maximum_m_index]}')
-    #     source_data[row].append(source_data[row][-1] / source_data[row][maximum_m_index])
-    #
-    s, r, c = transfiguration(source_data, index_row, index_col)
-    s1, r1, c1 = transfiguration(s, r, c)
-    s2, r2, c2 = transfiguration(s1, r1, c1)
-    s3, r3, c3 = transfiguration(s2, r2, c2)
-    transfiguration(s3, r3, c3)
+    transfiguration(source_data, index_row, index_col)
 
 
 if __name__ == '__main__':
