24de578894c649271c735058961643f251a2a546
Terentyev Alexey <alexey@terentyev.pro>
2019-09-24 23:16:02 +0700

    Lab3 refactor

--- 9_semester/theoryOfPL/lab3.py
+++ 9_semester/theoryOfPL/lab3.py
@@ -10,16 +10,96 @@ class SMM:
 
 
 class Rule:
-    def __init__(self, smm: SMM):
+    state_l = 'L'
+
+    def __init__(self, smm: SMM, state_current: str, chain_current: str, stack_current: str, state_new: str,
+                 stack_new: str):
         self._smm = smm
-        self.state_current = str()
-        self.chain_current = str()
-        self.stack_current = str()
-        self.state_new = str()
-        self.stack_new = str()
+        self.state_current = state_current
+        self.chain_current = chain_current
+        self.stack_current = stack_current
+        self.state_new = state_new
+        self.stack_new = stack_new
+        self._smm.rules.append(self)
+
+    def __del__(self):
+        self._smm.rules.remove(self)
+
+    def __str__(self):
+        return '"{%s, %s, %s} = {(%s, %s)}"' % (
+            self.state_current,
+            self.chain_current,
+            self.stack_current,
+            self.state_new,
+            self.stack_new
+        )
+
+    def __repr__(self):
+        return str(self)
+
+
+class Chain:
+    def __init__(self, smm: SMM, string: str):
+        self._smm = smm
+        self.string = string
+        self._history = list()
+        self.stack = self._smm.stack_initial
+        self.state = self._smm.state_initial
+
+    @property
+    def history(self):
+        return self._history
+
+    @history.setter
+    def history(self, value):
+        pass
+
+    def check(self) -> (bool, str):
+        message = str()
+        skip_flag = False
+        rule_found = False
+
+        while len(self.stack) > 0 and len(self.string) > 0:
+            if skip_flag:
+                skip_flag = False
+                continue
+            # else:
+            #     print('Current state: {}, current chain: {}, stack: {}'.format(self.state,
+            #                                                                    self.string,
+            #                                                                    self.stack))
+            for rule in self._smm.rules:
+                rule_found = False
+                if self.state == rule.state_current \
+                        and self.string[0] == rule.chain_current \
+                        and self.stack[0] == rule.stack_current:
+                    rule_found = True
+                    self.string = self.string[1:]
+                    self.history.append(rule)
+                    self.state = rule.state_new
+                    if rule.stack_new != rule.l:
+                        if len(rule.stack_new) == 1:
+                            self.stack = rule.stack_new + self.stack
+                        else:
+                            self.stack = rule.stack_new[:-1] + self.stack
+                    else:
+                        self.stack = self.stack[1:]
+                        skip_flag = True
+                    break
+            if not rule_found:
+                message = 'Rule not found'
+                break
+
+        if self.state not in self._smm.final_state_list:
+            message = 'Not in end state after chain end'
+        elif len(self.string) != 0 and len(self.stack):
+            message = 'Non-empty chain after stack end'
+        elif message == '':
+            message = 'Chain fits the machine'
+        return rule_found, message
 
 
 def _main():
+    # Mocked
     smm = SMM()
     smm.state_list = ['p', 'q']
     smm.alphabet = ['0', '1']
@@ -27,6 +107,16 @@ def _main():
     smm.state_initial = 'p'
     smm.stack_initial = 'Z'
     smm.final_state_list = ['p']
+    Rule(smm, 'p', '0', 'Z', 'p', '0Z')
+    Rule(smm, 'p', '0', '0', 'p', '00')
+    Rule(smm, 'p', '1', '0', 'q', 'L')
+    Rule(smm, 'p', 'L', 'Z', 'L', 'L')
+    Rule(smm, 'q', 'L', 'Z', 'q', 'L')
+    Rule(smm, 'q', '1', '0', 'p', 'L')
+    # print(smm.rules)
+    chain = Chain(smm, '00001111')
+    print(chain.check())
+    print(chain.history)
 
 
 if __name__ == '__main__':
