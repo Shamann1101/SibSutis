d120ede4a963e688b264b01448e0ad78ea1b371e
Alexey Terentyev <alexey@terentyev.pro>
2020-02-11 00:20:22 +0700

    Entropy for single symbols add

--- 10_semester/information_theory/kr1.py
+++ 10_semester/information_theory/kr1.py
@@ -1,6 +1,7 @@
 import random
 from os import stat
 
+import numpy as np
 
 FILE_SIZE = 10_000
 
@@ -15,12 +16,12 @@ def _get_result_vector(_dict: dict) -> tuple:
     if summation > 1.:
         raise ValueError('Value over 1')
 
-    x = 1. / summation
+    shift_coefficient = 1. / summation
     shift = 0.
 
     for symbol in _dict:
-        vector.append((_dict[symbol] * x + shift, symbol))
-        shift += _dict[symbol] * x
+        vector.append((_dict[symbol] * shift_coefficient + shift, symbol))
+        shift += _dict[symbol] * shift_coefficient
 
     return tuple(vector)
 
@@ -35,7 +36,7 @@ def _get_random_symbol(_dict: dict) -> str:
 
 def generate_file_1(title='file_1.txt', length=FILE_SIZE):
     with open(title, 'w+') as f:
-        letters = ('a', 'b', 'c')
+        letters = ('a', 'b', 'c', 'd')
         chance = 1. / len(letters)
 
         letters_dict = dict()
@@ -51,15 +52,39 @@ def generate_file_1(title='file_1.txt', length=FILE_SIZE):
 def generate_file_2(title='file_2.txt', length=FILE_SIZE):
     with open(title, 'w+') as f:
         letters = {
-            'a': 1 / 5,
-            'b': 1 / 2,
-            'c': 1 / 7,
+            'a': 0.51,
+            'b': 0.13,
+            'c': 0.08,
+            'd': 0.28,
         }
 
         while stat(title).st_size < length:
             f.write(_get_random_symbol(letters))
 
 
+def entropy_count(filename: str) -> float:
+    symbols_list = []
+
+    with open(filename, 'r') as f:
+        while True:
+            symbol = f.read(1)
+            if not symbol:
+                break
+            symbols_list.append(symbol)
+
+    symbols = np.array(symbols_list)
+
+    _, counts = np.unique(symbols, return_counts=True)
+    p = counts / np.sum(counts)
+
+    H = -np.sum(p * np.log2(p))
+
+    return H
+
+
 if __name__ == '__main__':
     generate_file_1()
     generate_file_2()
+
+    print(entropy_count('file_1.txt'))
+    print(entropy_count('file_2.txt'))
