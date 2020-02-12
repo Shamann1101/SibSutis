614d0327426de17fe179b301336f2ebd2f2a2569
Alexey Terentyev <alexey@terentyev.pro>
2020-02-12 21:00:15 +0700

    Redundancy count add

--- 10_semester/information_theory/kr1.py
+++ 10_semester/information_theory/kr1.py
@@ -3,8 +3,11 @@ from random import random
 
 import numpy as np
 
-FILE_SIZE = 10_000
-EXCLUDE_SYMBOLS = ('.', ',', '!', '?', '-', '^', ':', ';', '(', ')', '"', '\'', '\n', '\t')
+FILENAME_1 = 'file_1.txt'
+FILENAME_2 = 'file_2.txt'
+FILENAME_3 = 'file_3.txt'
+_FILE_SIZE = 10_000
+_EXCLUDE_SYMBOLS = ('.', ',', '!', '?', '-', '^', ':', ';', '(', ')', '"', '\'', '\n', '\t')
 
 
 def _get_result_vector(_dict: dict) -> tuple:
@@ -35,22 +38,18 @@ def _get_random_symbol(_dict: dict) -> str:
             return pair[1]
 
 
-def generate_file_1(title='file_1.txt', length=FILE_SIZE):
+def generate_file_1(title='file_1.txt', length=_FILE_SIZE):
     with open(title, 'w+') as f:
         letters = ('a', 'b', 'c', 'd')
         chance = 1. / len(letters)
 
-        letters_dict = dict()
-        letters_dict.fromkeys(letters)
-
-        for letter in letters:
-            letters_dict[letter] = chance
+        letters_dict = dict().fromkeys(letters, chance)
 
         while stat(title).st_size < length:
             f.write(_get_random_symbol(letters_dict))
 
 
-def generate_file_2(title='file_2.txt', length=FILE_SIZE):
+def generate_file_2(title='file_2.txt', length=_FILE_SIZE):
     with open(title, 'w+') as f:
         letters = {
             'a': 0.51,
@@ -63,20 +62,36 @@ def generate_file_2(title='file_2.txt', length=FILE_SIZE):
             f.write(_get_random_symbol(letters))
 
 
-def entropy_count(filename: str, size=1) -> float:
+def _string_prepare(filename: str) -> str:
     with open(filename, 'r') as f:
         file_str = f.read().lower()
 
-    for symbol in EXCLUDE_SYMBOLS:
+    for symbol in _EXCLUDE_SYMBOLS:
         file_str = file_str.replace(symbol, '+')
 
+    return file_str
+
+
+def _get_symbols_array(file_str: str, size: int) -> np.ndarray:
     symbols_list = []
+
     for i in range(len(file_str) - size + 1):
         symbols_list.append(file_str[i:i + size])
 
     symbols = np.array(symbols_list)
 
-    _, counts = np.unique(symbols, return_counts=True)
+    return symbols
+
+
+def entropy_count(filename: str, size=1, print_symbols=False) -> float:
+    file_str = _string_prepare(filename)
+    symbols = _get_symbols_array(file_str, size)
+
+    unique_symbols, counts = np.unique(symbols, return_counts=True)
+
+    if print_symbols:
+        print('for file {} get symbols: {}'.format(filename, unique_symbols))
+
     p = counts / np.sum(counts)
 
     H = -np.sum(p * np.log2(p)) / size
@@ -84,10 +99,40 @@ def entropy_count(filename: str, size=1) -> float:
     return H
 
 
-if __name__ == '__main__':
-    generate_file_1()
-    generate_file_2()
+def redundancy_count(filename: str, size=1) -> float:
+    file_str = _string_prepare(filename)
+    symbols = _get_symbols_array(file_str, size)
 
-    print(entropy_count('file_1.txt'))
-    print(entropy_count('file_2.txt'))
-    print(entropy_count('file_3.txt'))
+    _, counts = np.unique(symbols, return_counts=True)
+
+    H = entropy_count(filename, size)
+    H_max = np.log2(len(counts))
+    r = 1. - (H / H_max)
+
+    return r
+
+
+def _get_alphabet(filename: str, size=1) -> dict:
+    file_str = _string_prepare(filename)
+    symbols = _get_symbols_array(file_str, size)
+
+    unique_symbols, counts = np.unique(symbols, return_counts=True)
+    summation = np.sum(counts)
+
+    _dict = {unique_symbols[i]: counts[i] / summation for i in range(len(unique_symbols))}
+
+    return _dict
+
+
+if __name__ == '__main__':
+    generate_file_1(FILENAME_1)
+    generate_file_2(FILENAME_2)
+
+    print('entropy of file_1.txt', entropy_count(FILENAME_1))
+    print('redundancy of file_1.txt', redundancy_count(FILENAME_1))
+    print('entropy of file_2.txt', entropy_count(FILENAME_2))
+    print('redundancy of file_2.txt', redundancy_count(FILENAME_2))
+    print('entropy of file_3.txt', entropy_count(FILENAME_3))
+    print('redundancy of file_3.txt', redundancy_count(FILENAME_3))
+
+    print(_get_alphabet(FILENAME_1))
