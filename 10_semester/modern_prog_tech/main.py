ba3c42fbe06a3c7cbac974c4daae6899eb96b8b0
Alexey Terentyev <terentyevas@inetra.ru>
2020-03-03 22:24:43 +0700

    Button actions add

--- 10_semester/modern_prog_tech/main.py
+++ 10_semester/modern_prog_tech/main.py
@@ -2,101 +2,97 @@ import sys
 
 from PyQt5 import QtWidgets
 
-import rgr_calculator_design as design
+import calculator_design as design
+from calculator_controller import CalculatorController
 
 
-# TODO: Memory using
-# TODO: Buffer using
-# TODO: History using
-
-
-class Calculator:
-    def __init__(self, base: int):
-        self.precision = 2 ** -10
-        self.stored_value = '0'
-        self._current_value = '0'
-
-        if base < 2 or base > 16:
-            raise ValueError('Base value error')
-        self._base = base
-
-    @property
-    def base(self) -> int:
-        return self._base
-
-    @base.setter
-    def base(self, value: int):
-        self.stored_value = self._convert_base(self.stored_value, to_base=value, from_base=self._base)
-        self.current_value = self._convert_base(self.current_value, to_base=value, from_base=self._base)
-        self._base = value  # FIXME
-        print('stored: {}, current: {}'.format(self.stored_value, self.current_value))
-
-    @property
-    def current_value(self) -> str:
-        return self._current_value
-
-    @current_value.setter
-    def current_value(self, value):  # FIXME
-        self._current_value = value
-
-    @staticmethod
-    def _convert_base(value: int or str, to_base=10, from_base=10) -> str:
-        if isinstance(value, str):
-            n = int(value, from_base)
-        else:
-            n = int(value)
-        alphabet = '0123456789ABCDEF'
-        if n < to_base:
-            return alphabet[n]
-        else:
-            return Calculator._convert_base(n // to_base, to_base) + alphabet[n % to_base]
+# TODO: Add switch to lastLineEdit
+# TODO: Add 'About' window
 
 
 class CalculatorApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.lastLineEdit = self.lineEdit_first
 
-        self.calc = Calculator(base=self.slider.value())
+        self.calc = CalculatorController()
+        # self.lineEdit_first.selectedText.connect(self.lineEdit_first_action)
 
-        self.lineEdit.setText(str(self.calc.stored_value))
-
-        self.lcdNumber.display(self.slider.value())
-        self.slider.valueChanged.connect(self.update_base)
+        self.btnResult.clicked.connect(self.btnResult_action)
 
         for button in self.findChildren(QtWidgets.QAbstractButton):
-            # action = getattr(globals()['CalculatorApp'](), 'btn{}_action'.format(button.text()), None)
-            action = getattr(CalculatorApp, 'btn{}_action'.format(button.text()), None)
+            action = getattr(self, '{}_action'.format(button.objectName()), None)
             if action:
-                pass
-                # button.clicked.connect(action)
-                # print(action)
+                button.clicked.connect(action)
 
-        self.btnA.clicked.connect(self.btnA_action)
-        self._disable_buttons()
+    def lineEdit_first_action(self):
+        self.lastLineEdit = self.lineEdit_first
 
-    def _disable_buttons(self):
-        base_list = [i for i in range(2, 10, 1)]
-        base_list.extend(['A', 'B', 'C', 'D', 'E', 'F'])
-        for i in range(2, 16, 1):
-            if i >= self.calc.base:
-                self._disable_button('btn{}'.format(base_list[i-2]))
-            else:
-                self._disable_button('btn{}'.format(base_list[i-2]), disable=False)
+    def btn0_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '0')
 
-    def _disable_button(self, btn_name: str, disable=True):
-        btn = self.findChild(QtWidgets.QAbstractButton, btn_name)
-        btn.setDisabled(disable)
+    def btn1_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '1')
 
-    def update_base(self):
-        self.calc.base = self.slider.value()
-        self.lineEdit.setText(str(self.calc.current_value))
-        self.lcdNumber.display(self.slider.value())
-        self._disable_buttons()
+    def btn2_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '2')
 
-    def btnA_action(self):
-        self.calc.current_value += 'A'
-        self.lineEdit.setText(str(self.calc.current_value))
+    def btn3_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '3')
+
+    def btn4_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '4')
+
+    def btn5_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '5')
+
+    def btn6_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '6')
+
+    def btn7_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '7')
+
+    def btn8_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '8')
+
+    def btn9_action(self):
+        self.lastLineEdit.setText(self.lineEdit_first.text() + '9')
+
+    def btnBS_action(self):
+        self.lastLineEdit.setText(self.lastLineEdit.text()[:-1])
+
+    def btnC0_action(self):
+        self.lastLineEdit.setText('')
+
+    def btnCE_action(self):
+        self.lastLineEdit = self.lineEdit_first
+        self.lineEdit_first.setText('')
+        self.lineEdit_second.setText('')
+        self.lineEdit_result.setText('')
+
+    def btnSign_action(self):
+        if self.lastLineEdit.text()[0] == '-':
+            self.lastLineEdit.setText(self.lastLineEdit.text()[1:])
+        else:
+            self.lastLineEdit.setText('-' + self.lastLineEdit.text())
+
+    def btnSlash_action(self):
+        if '/' not in self.lastLineEdit.text():
+            self.lastLineEdit.setText(self.lastLineEdit.text() + '/')
+
+    def btnResult_action(self):
+        action = ''
+        if self.radioDiv.isChecked():
+            action = '/'
+        elif self.radioMul.isChecked():
+            action = '*'
+        elif self.radioSub.isChecked():
+            action = '-'
+        elif self.radioAdd.isChecked():
+            action = '+'
+
+        self.lineEdit_result.setText(self.calc.action(self.lineEdit_first.text(), self.lineEdit_second.text(), action))
 
 
 def main():
