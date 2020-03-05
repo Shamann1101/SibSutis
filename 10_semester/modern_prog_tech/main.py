5bfaa3c563da5474915bae6d36344d54805f5cc1
Alexey Terentyev <alexey@terentyev.pro>
2020-03-05 14:27:03 +0700

    Input validation add

--- 10_semester/modern_prog_tech/main.py
+++ 10_semester/modern_prog_tech/main.py
@@ -1,12 +1,24 @@
+import re
 import sys
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtGui, QtWidgets
 
 import calculator_design as design
 from calculator_controller import CalculatorController
 
 
 # TODO: Add switch to lastLineEdit
+
+
+class InputValidator(QtGui.QValidator):
+    def __init__(self, parent=None):
+        super(InputValidator, self).__init__(parent)
+        self._replace = re.compile(r'[^0-9/\-]').sub
+        # self._re = re.compile(r'^(-?)(\d*)((/)(\d+))?$').sub
+
+    def validate(self, string, pos):
+        string = self._replace('', string)
+        return QtGui.QValidator.Acceptable, string, pos
 
 
 class CalculatorApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
@@ -17,7 +29,10 @@ class CalculatorApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
         self.calc = CalculatorController()
         self.actionAbout.triggered.connect(self.actionAbout_action)
-        # self.lineEdit_first.selectedText.connect(self.lineEdit_first_action)
+        self.lineEdit_first.setValidator(InputValidator(self))
+        self.lineEdit_first.returnPressed.connect(self.lastLineEdit_action)
+        self.lineEdit_second.setValidator(InputValidator(self))
+        self.lineEdit_second.returnPressed.connect(self.lastLineEdit_action)
 
         self.btnResult.clicked.connect(self.btnResult_action)
 
@@ -33,38 +48,38 @@ class CalculatorApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         msg.setWindowTitle("CalculatorApp demo")
         msg.exec_()
 
-    def lineEdit_first_action(self):
-        self.lastLineEdit = self.lineEdit_first
+    def lastLineEdit_action(self):
+        self.lastLineEdit = self.sender()
 
     def btn0_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '0')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '0')
 
     def btn1_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '1')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '1')
 
     def btn2_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '2')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '2')
 
     def btn3_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '3')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '3')
 
     def btn4_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '4')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '4')
 
     def btn5_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '5')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '5')
 
     def btn6_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '6')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '6')
 
     def btn7_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '7')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '7')
 
     def btn8_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '8')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '8')
 
     def btn9_action(self):
-        self.lastLineEdit.setText(self.lineEdit_first.text() + '9')
+        self.lastLineEdit.setText(self.lastLineEdit.text() + '9')
 
     def btnBS_action(self):
         self.lastLineEdit.setText(self.lastLineEdit.text()[:-1])
