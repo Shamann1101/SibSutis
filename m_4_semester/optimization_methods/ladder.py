5e2bdb7f9a0b70a2f39eb392c4056472071da584
Alexey Terentyev <alexey@terentyev.pro>
2023-02-21 22:16:23 +0700

    Fix ladder

--- m_4_semester/optimization_methods/ladder.py
+++ m_4_semester/optimization_methods/ladder.py
@@ -12,9 +12,22 @@ def ladder(_in: list) -> int:
     return _list[-1]
 
 
+def _get_list(filename: str = 'ladder.in') -> list:
+    with open(filename, 'r') as f:
+        count = int(f.readline())
+        value_list = f.readline().split(' ', count)
+        return [int(v.strip()) for v in value_list]
+
+
+def _save_result(value: int, filename: str = 'ladder.out'):
+    with open(filename, 'w') as f:
+        f.write(str(value))
+
+
 def main():
-    _list = [3, 1, -4, 1, 5, 9, -2, -6]
-    print(ladder(_list))
+    _list = _get_list()
+    result = ladder(_list)
+    _save_result(result)
 
 
 if __name__ == '__main__':
